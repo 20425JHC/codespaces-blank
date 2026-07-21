b8c57204d77492ae4907288f42e824fae97c130f
Saranya Sarisa <20425@jhc.school.nz>
2026-07-21 01:26:45 +0000

    modified:   goodviews/db.sqlite3 	modified:   goodviews/goodviews/__pycache__/settings.cpython-312.pyc 	modified:   goodviews/goodviews/__pycache__/urls.cpython-312.pyc 	modified:   goodviews/goodviews/settings.py 	modified:   goodviews/goodviews/urls.py 	new file:   goodviews/media/__pycache__/__init__.cpython-312.pyc 	new file:   goodviews/media/__pycache__/admin.cpython-312.pyc 	new file:   goodviews/media/__pycache__/apps.cpython-312.pyc 	new file:   goodviews/media/__pycache__/models.cpython-312.pyc 	new file:   goodviews/media/__pycache__/urls.cpython-312.pyc 	new file:   goodviews/media/__pycache__/views.cpython-312.pyc 	new file:   goodviews/media/migrations/__pycache__/__init__.cpython-312.pyc 	new file:   goodviews/media/templates/myfirst.html 	new file:   goodviews/media/urls.py 	modified:   goodviews/media/views.py

--- goodviews/media/views.py
+++ goodviews/media/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def media(request):
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
