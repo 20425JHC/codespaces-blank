ccbca9c997e501c2684e228a9c529e033ba56393
Saranya Sarisa <20425@jhc.school.nz>
2026-08-15 21:20:50 +0000

    modified:   goodviews/media/__pycache__/urls.cpython-312.pyc 	modified:   goodviews/media/__pycache__/views.cpython-312.pyc 	new file:   goodviews/media/static/styles/style.css 	new file:   goodviews/media/templates/base.html 	new file:   goodviews/media/templates/films.html 	modified:   goodviews/media/templates/home.html 	new file:   goodviews/media/templates/sign_up.html 	new file:   goodviews/media/templates/tv_shows.html 	modified:   goodviews/media/urls.py 	modified:   goodviews/media/views.py

--- goodviews/media/urls.py
+++ goodviews/media/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('media/', views.media, name='media'),
+    path('', views.home, name='home'),
+    path('films/', views.films, name='films'),
+    path('tv_shows/', views.tv_shows, name='tv_shows'),
+    path('sign_up/', views.sign_up, name='sign_up'),
 ]
--- goodviews/media/views.py
+++ goodviews/media/views.py
@@ -1,6 +1,26 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
+from .models import films as FilmsModel, tv_shows as TVShowsModel
 
-def media(request):
-  template = loader.get_template('home.html')
-  return HttpResponse(template.render())
+#home page
+def home(request):
+    return render(request, 'home.html')
+
+#films page
+def films(request):
+    all_films = FilmsModel.objects.all()
+    context = {
+        'films': all_films,
+    }
+    return render(request, 'films.html', context)
+
+#tv shows page
+def tv_shows(request):
+    all_tv_shows = TVShowsModel.objects.all()
+    context = {
+        'tv_shows': all_tv_shows,
+    }
+    return render(request, 'tv_shows.html', context)
+
+#sign up page:
+def sign_up(request):
+    return render(request, 'sign_up.html')
