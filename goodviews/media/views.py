d12227e9e4b055060847bfd54e2244cab8432f6f
Saranya Sarisa <20425@jhc.school.nz>
2026-08-23 04:21:37 +0000

    modified:   goodviews/films.csv 	modified:   goodviews/media/__pycache__/views.cpython-312.pyc 	modified:   goodviews/media/templates/films.html 	modified:   goodviews/media/templates/tv_shows.html 	modified:   goodviews/media/views.py

--- goodviews/media/views.py
+++ goodviews/media/views.py
@@ -11,18 +11,38 @@ def goodviews(request):
 
 #films page
 def films(request):
-    all_films = FilmsModel.objects.all()
+    selected_genre = request.GET.get('genre')
+    selected_age_rating = request.GET.get('age_rating')
+
+    if selected_genre:
+        #filter films using foreign key in genre
+        all_films = FilmsModel.objects.filter(genre__genre__iexact=selected_genre)
+    if selected_age_rating:
+        all_films = FilmsModel.objects.filter(age_rating__age_rating__iexact=selected_age_rating)
+    else:
+        all_films = FilmsModel.objects.all()
+
     context = {
         'films': all_films,
+        'selected_genre': selected_genre,
+        'selected_age_rating': selected_age_rating, 
     }
     return render(request, 'films.html', context)
 
 #tv shows page
 def tv_shows(request):
-    all_tv_shows = TVShowsModel.objects.all()
+    selected_genre = request.GET.get('genre')
+    
+    if selected_genre:
+            #filter shows using foreign key in genre
+        all_tv_shows = TVShowsModel.objects.filter(genre__genre__iexact=selected_genre)
+    else:
+        all_tv_shows = TVShowsModel.objects.all()
+    
     context = {
         'tv_shows': all_tv_shows,
-    }
+        'selected_genre': selected_genre, 
+        }
     return render(request, 'tv_shows.html', context)
 
 #sign up page:
